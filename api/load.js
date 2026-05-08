ffe443dca7d708f5ffdd33f4bbbd1d2dc388c56c
andrepvlk2612 <agpaveloski@orizon.com.br>
2026-05-08 17:44:51 -0300

    Update load.js

--- api/load.js
+++ api/load.js
@@ -1,14 +1,12 @@
 export default async function handler(req, res) {
   const GH_TOKEN = process.env.GH_TOKEN;
   const GH_API = 'https://api.github.com/repos/andrepvlk2612/cx_orizon/contents/dados.json';
-
   const r = await fetch(GH_API, {
     headers: {
       Authorization: `token ${GH_TOKEN}`,
       Accept: 'application/vnd.github.v3+json'
     }
   });
-
   const data = await r.json();
   res.status(r.status).json(data);
 }
